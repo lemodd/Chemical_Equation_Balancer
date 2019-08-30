1230426f1ba2ac0facc81385c7f27b0b2758f32f
lemodd <lemodd@qq.com>
2019-08-30 13:16:41 +0800

    Update CEB_GUI.py

--- CEB_GUI.py
+++ CEB_GUI.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\python_dell\Chemical_Equation_Balancer\GUI.ui'
+# autohor:lemodd
+# mail:lemodd@qq.com
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
-# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import utils
